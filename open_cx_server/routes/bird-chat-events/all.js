002cf7e0b320649ce7693071172b43aa9a993940
Moisés Rocha <moisesrocha99@gmail.com>
2019-11-30 16:43:47 +0000

    event routers

--- open_cx_server/routes/bird-chat-events/all.js
+++ open_cx_server/routes/bird-chat-events/all.js
@@ -14,7 +14,22 @@ allBCEvents.get('/', async (req, res) => {
 
 allBCEvents.post('/', async (req, res) => {
   console.log(req.body);
-  res.send("psot on bcevents");
+
+  const event = new BCEvent({
+    creator: req.body.creator,
+    title: req.body.title,
+    location: req.body.location,
+    startTime: req.body.starttime,
+    description: req.body.description
+  });
+
+  event.save((err) => {
+    if (err) {
+      return res.status(400).send('Bad form parameters.');
+    } else {
+      return res.send(req.body);
+    }
+  });
 });
 
 module.exports = allBCEvents;
--- open_cx_server/routes/bird-chat-events/single.js
+++ open_cx_server/routes/bird-chat-events/single.js
@@ -1,13 +1,41 @@
-let Event = require('../../models/bird-chat-event');
+let BCEvent = require('../../models/bird-chat-event');
 
 const singleEvent = require('express').Router();
 
 singleEvent.get('/:postId', async (req, res) => {
-  res.send("Get request on /events/id");
+  try {
+    const event = await BCEvent.findOne({
+      _id: req.params.postId
+    });
+
+    res.json(event);
+  } catch (err) {
+    res.status(400).json({error : err});
+  }
 });
 
 singleEvent.post('/:postId', async (req, res) => {
-  res.send(req.params);
+
+  const event = await BCEvent.findOne({
+    _id: req.params.postId
+  });
+
+  if (!event) return res.status(400).send('Event doesn\'t exist');
+
+  event.creator = req.body.creator;
+  event.title = req.body.title;
+  event.location = req.body.location;
+  event.startTime = req.body.starttime;
+  event.description = req.body.description;
+
+  event.save((err) => {
+    if (err) {
+      res.status(400).json({error: err})
+    } else {
+      res.json(event);
+    }
+  });
+
 });
 
 module.exports = singleEvent;
